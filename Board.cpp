c5ddf3274dee6b4d2b6677b76eb4d966dbcf4612
ahmadml <57864630+ahmadml@users.noreply.github.com>
2020-06-04 20:14:03 +0300

    Update Board.cpp

--- Board.cpp
+++ Board.cpp
@@ -17,9 +17,9 @@ namespace WarGame {
             return  board[location.first][location.second];
         }
 
-       void Board::move(uint player_number, std::pair<int,int> source, MoveDIR direction) {
+        void Board::move(uint player_number, std::pair<int,int> source, MoveDIR direction) {
             std::pair<int,int> temp = source;
-            if(source.first>=board.size() || source.second>=board[0].size()) throw out_of_range{"error"};
+            if(source.first>=board.size() || source.second>=board[0].size() || source.first<0 || source.second<0) throw out_of_range{"error"};
             Soldier* sol = board[source.first][source.second];
             if(sol!=nullptr && sol->nop==player_number){
                 if(direction==Up) temp.first+=1;
@@ -30,7 +30,7 @@ namespace WarGame {
                     board[temp.first][temp.second]=board[source.first][source.second];
                     board[source.first][source.second]=nullptr;
                     sol = board[temp.first][temp.second];
-                    //sol->attak(source,this->board,player_number);
+                    sol->attak(temp,this->board,player_number);
                 }
                else throw out_of_range{"error"};
             }
